bc77f8b23c016b008d2ec48d5335ace793a17995
Andreas S K <andreas.syaloom.k@gmail.com>
2021-07-15 23:48:11 +0700

    zxzx
    xzx

---
+++ notebook.jl
@@ -0,0 +1,23 @@
+### A Pluto.jl notebook ###
+# v0.14.5
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ d5b8cb71-4d08-4f02-8551-26d1f80ef72e
+
+
+# ╔═╡ ea51a980-e58a-11eb-3906-eb6aa150db3d
+1+1
+
+# ╔═╡ 57581803-37b9-4d80-9926-c23813501eef
+sqrt(5)
+
+# ╔═╡ 0bb6eb91-e624-42c3-84c1-f9fea76f8b26
+log(54)
+
+# ╔═╡ Cell order:
+# ╠═d5b8cb71-4d08-4f02-8551-26d1f80ef72e
+# ╠═ea51a980-e58a-11eb-3906-eb6aa150db3d
+# ╠═57581803-37b9-4d80-9926-c23813501eef
+# ╠═0bb6eb91-e624-42c3-84c1-f9fea76f8b26
